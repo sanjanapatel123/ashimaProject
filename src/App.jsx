87a84d4e0479f201bac308d9024752100b752a09
Shraddha-Choudhary <shraddha.chaudhary000@gmail.com>
2025-03-01 11:15:17 +0530

    shraddha

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <LearningJourney />
       <LearningStyle />
       <Ride />
-      <CategoryButtons />
+      <CategoryButtons /> 
       <FeaturedIn />
       <StartLearning />
       <DigitalProducts />
