315c5a888bd9f813ba24a55e3d6c896bd91a91b0
ankitverma3490 <ankitverma3490@gmail.com>
2025-04-17 10:49:32 +0530

    redux setuo

--- src/components/Redux/store.js
+++ src/components/Redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 // import your reducers here
-import exampleReducer from '../features/example/exampleSlice'
+// import exampleReducer from '../features/example/exampleSlice'
 import addStudentReducer from './slices/adminSlices/addStudentSlice'
 export const store = configureStore({
   reducer: {
